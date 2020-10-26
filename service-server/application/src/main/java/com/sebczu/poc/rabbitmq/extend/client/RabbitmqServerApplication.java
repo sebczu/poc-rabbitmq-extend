c8bfdb680a3f751db263a7ebed3128ff739a3a02
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-10-26 21:57:26 +0100

    client and server

---
+++ service-server/application/src/main/java/com/sebczu/poc/rabbitmq/extend/client/RabbitmqServerApplication.java
@@ -0,0 +1,13 @@
+package com.sebczu.poc.rabbitmq.extend.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqServerApplication.class, args);
+    }
+
+}
